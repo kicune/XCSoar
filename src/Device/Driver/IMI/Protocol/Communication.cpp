c6bc2573f13dc433ddb7aed213a249b342f50c66
Honza Liška <jan.liska@cleverlance.com>
2023-06-17 22:37:48 +0200

    Fix for LKBU version - IMI driver

--- src/Device/Driver/IMI/Protocol/Communication.cpp
+++ src/Device/Driver/IMI/Protocol/Communication.cpp
@@ -20,7 +20,7 @@ namespace IMI
   extern IMIWORD _serialNumber;
 }
 
-bool
+void
 IMI::Send(Port &port, OperationEnvironment &env,
           IMIBYTE msgID, std::span<const std::byte> payload,
           IMIBYTE parameter1, IMIWORD parameter2, IMIWORD parameter3)
