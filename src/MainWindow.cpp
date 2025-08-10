a3accc2c1ed413c9c4db71183f9cc185468b4724
Jan Liska <honza.liska@gmail.com>
2025-08-10 23:29:40 +0200

    custom DPI test

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -222,6 +222,7 @@ MainWindow::InitialiseConfigured()
   const UISettings &ui_settings = CommonInterface::GetUISettings();
 
   LogFmt("Calling Initialise from InitialiseConfigured");
+  LogFmt("Custom dpi: {}", CommonInterface::GetUISettings().custom_dpi);
   if ((ui_settings.scale != 100) || (ui_settings.info_boxes.scale_title_font != 100))
     /* call Initialise() again to reload fonts with the new scale */
     Initialise();
